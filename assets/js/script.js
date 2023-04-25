6fc43a6c3f1ad82b02fd8f95ca228809a1037247
HectorJosueDH <hectorjosue78@gmail.com>
2023-04-24 21:24:35 -0600

    Added some scripts - unfinished

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,29 @@
-var api_KEY = '905c5677ef9598108ef712c3c6f027d0';
-LAT = 44;
-LON = 44;
-var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + LAT + '&lon=' + LON + '&appid=' + api_KEY;
+var API_KEY = '905c5677ef9598108ef712c3c6f027d0';
+// New York coords
 
-fetch(queryURL)
+// variables
+let city = '';
+let searchCity = $("#search-city");
+let searchButton = $("#search-button");
+let currentCity = $("#current-city");
+let currentTemperature = $("#temperature");
+let currentHumidty = $("#humidity");
+let currentWSpeed = $("#wind");
+let sCity=[];
+
+function displayWeather(event){
+  event.preventDefault();
+  if(searchCity.val().trim()!==""){
+    city=searchCity.val().trim();
+    currentWeather(city);
+  }
+}
+
+function weather(city) {
+  var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + LAT + '&lon=' + LON + '&appid=' + API_KEY;
+  
+  fetch(queryURL)
+  .then(function (response) {
+    console.log(response);
+  })
+}
