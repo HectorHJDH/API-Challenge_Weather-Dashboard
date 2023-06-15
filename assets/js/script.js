88c0158ec4097790f4ebdff08f53e26c386a1346
HectorJosueDH <hectorjosue78@gmail.com>
2023-06-14 18:03:21 -0600

    Starting again from scratch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,21 @@
-var API_KEY = '905c5677ef9598108ef712c3c6f027d0';
-// New York coords
+var API_KEY = process.env.API_KEY;
+let searchCity = document.getElementById("search-city");
+let searchBtn = document.querySelector(".search-button");
+let weatherData = document.getElementById("weather-data");
+// api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
+// Do a fetch 
+// Create a lat and lon 
+// Create the html cards with a for loop in javascript
+// Get the data to show up correctly
 
-// variables
-let city = '';
-let searchCity = $("#search-city");
-let searchButton = $("#search-button");
-let currentCity = $("#current-city");
-let currentTemperature = $("#temperature");
-let currentHumidty = $("#humidity");
-let currentWSpeed = $("#wind");
-let sCity=[];
-
-function displayWeather(event){
-  event.preventDefault();
-  if(searchCity.val().trim()!==""){
-    city=searchCity.val().trim();
-    currentWeather(city);
-  }
-}
-
-function weather(city) {
-  var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + LAT + '&lon=' + LON + '&appid=' + API_KEY;
+function getApi() {
   
-  fetch(queryURL)
-  .then(function (response) {
-    console.log(response);
-  })
 }
+
+
+getApi();
+
+
+
+
+
